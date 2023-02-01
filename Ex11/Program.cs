41570f07e845f0172f961557231c8250cf26a186
drkop <satanatyn@yandex.ru>
2023-02-01 22:20:07 +0300

    Ex11

---
+++ Ex11/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+if (number.Length > 2 ) Console.WriteLine(number[2]);
+else Console.WriteLine("У числа нет третьей цифры");
