0c9ef5cc853533db8fa207401edafd8bde2a14c0
drkop <satanatyn@yandex.ru>
2023-02-01 17:34:50 +0300

    Ex5

---
+++ Ex5/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int index = 0;
+while (index < array.Length)
+{
+    if (array[index] % 2 == 0) 
+    {
+        Console.WriteLine(array[index]);
+    }
+    index++;
+}
