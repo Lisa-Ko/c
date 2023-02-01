c528272b9c4196f60d53d5e8154a8f32719b21d9
drkop <satanatyn@yandex.ru>
2023-02-01 21:57:47 +0300

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число "); //заданное число 17
+int number = int.Parse(Console.ReadLine());
+if (number % 17 == 0) Console.WriteLine("Число кратно 17");
+else Console.Write("Число не кратно 17, а остаток равен " + number % 17);
