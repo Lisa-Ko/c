731a5e77ad6284eb98bdbc0fa7e799e1c2e01ccf
drkop <satanatyn@yandex.ru>
2023-02-01 13:52:24 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 3 числа ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+Console.WriteLine("Максимальное значение ");
+Console.WriteLine(max);
